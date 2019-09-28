3a5b1283e4443f5dd443be50d89921c1fb4796d3
chiju <chiju@devopsnexus.com>
2019-09-28 16:20:16 +0530

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,10 @@ cd rpms
 for f in $(find -name "*.rpm" -type f)
 do 
 	echo "Uploading" 
-	curl -v  -X PUT -T $f -u chiju:32ea322465698d89b0b2844be7d8007527516e6e -H "X-Bintray-Version:$version" -H "X-Bintray-Package:$package" https://api.bintray.com/content/$username/$repository/
+	curl -v  -X PUT -T $f -u chiju:32ea322465698d89b0b2844be7d8007527516e6e \
+												-H "X-Bintray-Version:$version" \
+												-H "X-Bintray-Package:$package" \
+												https://api.bintray.com/content/$username/$repository
 done
 
 
