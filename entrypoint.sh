860c81e6e65a621e5f9719e1b78b53059db9e41a
chiju <chiju@devopsnexus.com>
2019-09-28 15:52:44 +0530

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=7.8
+version=7.6
 username='chiju'
 password=$bintray_key
 repository='rpms'
@@ -17,7 +17,11 @@ git clone $git_repo
 cd rpms
 
 #upoloading pacakges
-for f in $(find -name "*.rpm" -type f); do echo "Uploading"; curl -v  -X PUT -T $f -u chiju:32ea322465698d89b0b2844be7d8007527516e6e -H "X-Bintray-Version:7.8" -H "X-Bintray-Package:openssh" https://api.bintray.com/content/chiju/rpms/; echo; echo; done
+for f in $(find -name "*.rpm" -type f)
+do 
+	echo "Uploading" 
+	curl -v  -X PUT -T $f -u chiju:32ea322465698d89b0b2844be7d8007527516e6e -H "X-Bintray-Version:7.8" -H "X-Bintray-Package:openssh" https://api.bintray.com/content/chiju/rpms/
+done
 
 # Publishing the version
 curl -svvf -u $username:$password \
